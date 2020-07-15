107359b4aac54ae3e152a02eca5e6cb8446e36a7
tanzilislam <tanjilislam90@gmail.com>
2020-07-15 22:50:15 +0600

    update

--- components/user/NavBar.vue
+++ components/user/NavBar.vue
@@ -18,13 +18,39 @@
         <b-navbar-nav class="ml-auto">
           <!-- <b-nav-form> -->
           <b-input-group class="search-bar">
-            <b-form-input placeholder="Search Here..."> </b-form-input>
+            <b-form-input
+              @click="showDropdown = true"
+              placeholder="Search Here..."
+            >
+            </b-form-input>
             <b-input-group-append>
               <b-button variant="dark" style="width:70px;" size="md">
                 <b-icon variant="light" scale="1" icon="search"></b-icon>
               </b-button>
             </b-input-group-append>
           </b-input-group>
+
+          <div class="ml-4" v-if="showDropdown">
+            <div class="d-flex w-100">
+              <div class="c-dropdown">
+                <b-form-select
+                  class="my-select"
+                  v-model="selected"
+                  :options="options"
+                ></b-form-select>
+              </div>
+              <div class="ml-3 clos-icon">
+                <b-icon
+                  icon="x"
+                  @click="showDropdown = false"
+                  variant="light"
+                  scale="2"
+                  class="mt-2"
+                  style="cursor:pointer;"
+                ></b-icon>
+              </div>
+            </div>
+          </div>
           <!-- </b-nav-form> -->
         </b-navbar-nav>
         <!-- </b-collapse> -->
@@ -35,7 +61,24 @@
 
 <script>
 export default {
-  name: "NavBar"
+  name: "NavBar",
+  data() {
+    return {
+      showDropdown: false,
+      selected: null,
+      options: [
+        { value: null, text: "All" },
+        { value: "programming", text: "Programming" },
+        { value: "mobile", text: "Mobile Phone" },
+        { value: "celebrityAndMovie", text: "Celebrity & Movie" },
+        { value: "gaming", text: "Gaming" },
+        { value: "history", text: "History" },
+        { value: "health", text: "Health" },
+        { value: "technology", text: "Technology" },
+        { value: "designLibrary", text: "Design Library" }
+      ]
+    };
+  }
 };
 </script>
 
@@ -64,9 +107,12 @@ a {
 a {
   border: none !important;
   outline: none !important;
-
   outline-style: none !important;
 }
+.my-select {
+  width: auto;
+}
+
 /* .custom-navbar {
 
 /* } */
