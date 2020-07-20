5ccdbace9fcbcd54b6bc71140d9a778ca537761c
tanzilislam <tanjilislam90@gmail.com>
2020-07-21 02:41:14 +0600

    update

--- components/user/Carousel.vue
+++ components/user/Carousel.vue
@@ -6,16 +6,22 @@
       :interval="4000"
       controls
       indicators
-      background="#ababab"
+      background="#fafafa"
       img-width="1024"
-      img-height="300"
-      style="text-shadow: 1px 1px 2px #333;"
+      img-height="200"
+      style="text-shadow: 1px 1px 2px #333; "
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <b-carousel-slide img-blank img-alt="Blank image"> </b-carousel-slide>
-      <b-carousel-slide img-blank img-alt="Blank image"> </b-carousel-slide>
-      <b-carousel-slide img-blank img-alt="Blank image"> </b-carousel-slide>
+      <b-carousel-slide
+        class="cover-img rounded"
+        :img-src="i.Cover_img"
+        img-alt="Blank image"
+        img-height="200"
+        v-for="(i, index) in data"
+        :key="index"
+      >
+      </b-carousel-slide>
     </b-carousel>
   </div>
 </template>
@@ -26,7 +32,8 @@ export default {
   data() {
     return {
       slide: 0,
-      sliding: null
+      sliding: null,
+      data: []
     };
   },
   methods: {
@@ -36,6 +43,11 @@ export default {
     onSlideEnd(slide) {
       this.sliding = false;
     }
+  },
+  mounted() {
+    this.$axios
+      .$get(process.env.baseUrl + `/Coverimgapi`)
+      .then(posts => (this.data = posts));
   }
 };
 </script>
@@ -43,5 +55,9 @@ export default {
 <style scoped>
 .myCarousel {
   margin-bottom: 10px;
+}
+.cover-img {
+  height: 300px;
+  border-radius: 10px !important;
 }
 </style>
